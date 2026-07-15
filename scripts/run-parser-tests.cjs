c1f10846876ba1751e6cedc7395038ddf2941bcc
Baljinder Singh <sandhubaljinder003@gmail.com>
2026-07-15 14:21:52 +0530

    Generate Prisma Client to custom prisma-client directory to bypass ASAR dot-folder restriction

--- scripts/run-parser-tests.cjs
+++ scripts/run-parser-tests.cjs
@@ -1,4 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('prisma-client');
 const { parseReport } = require('../dist-electron/src/lib/parser/pipeline');
 const { SubmissionRepository } = require('../dist-electron/src/repositories/SubmissionRepository');
 
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from 'prisma-client';
 import path from 'path';
 import fs from 'fs';
 
